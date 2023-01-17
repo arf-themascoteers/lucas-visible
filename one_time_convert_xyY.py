4d6abf7baa2a5a45f8eabeca23b353f4ca5f726e
Arif <80994072+arf-themascoteers@users.noreply.github.com>
2023-01-18 04:09:18 +1100

    done

--- one_time_convert_xyY.py
+++ one_time_convert_xyY.py
@@ -3,30 +3,35 @@ import colour
 import numpy as np
 
 
-df = pd.read_csv("data_XYZ_original.csv")
-npdf = df.to_numpy()
-for i in range(len(npdf)):
-    npdf[i,0:3] = colour.XYZ_to_xyY(npdf[i,0:3])
-print("Original")
-print(npdf[:, 0].min())
-print(npdf[:, 1].min())
-print(npdf[:, 2].min())
-print(npdf[:, 0].max())
-print(npdf[:, 1].max())
-print(npdf[:, 2].max())
-df = pd.DataFrame(npdf, columns = ['x','y','Y','oc'])
-df.to_csv("data_xyY_original.csv", index=False)
+def process(inf, outf_original, outf):
+    df = pd.read_csv(inf)
+    npdf = df.to_numpy()
+    for i in range(len(npdf)):
+        npdf[i,0:3] = colour.XYZ_to_xyY(npdf[i,0:3])
+    print("Original")
+    print(npdf[:, 0].min())
+    print(npdf[:, 1].min())
+    print(npdf[:, 2].min())
+    print(npdf[:, 0].max())
+    print(npdf[:, 1].max())
+    print(npdf[:, 2].max())
+    df = pd.DataFrame(npdf, columns = ['x','y','Y','oc'])
+    df.to_csv(outf_original, index=False)
 
-max_values = np.max(npdf[:, 0:3])
-npdf[:, 0:3] = npdf[:, 0:3] / max_values
-print("Normalized")
-print(npdf[:, 0].min())
-print(npdf[:, 1].min())
-print(npdf[:, 2].min())
-print(npdf[:, 0].max())
-print(npdf[:, 1].max())
-print(npdf[:, 2].max())
-df = pd.DataFrame(npdf, columns = ['x','y','Y','oc'])
-df.to_csv("data_xyY.csv", index=False)
+    max_values = np.max(npdf[:, 0:3])
+    npdf[:, 0:3] = npdf[:, 0:3] / max_values
+    print("Normalized")
+    print(npdf[:, 0].min())
+    print(npdf[:, 1].min())
+    print(npdf[:, 2].min())
+    print(npdf[:, 0].max())
+    print(npdf[:, 1].max())
+    print(npdf[:, 2].max())
+    df = pd.DataFrame(npdf, columns = ['x','y','Y','oc'])
+    df.to_csv(outf, index=False)
 
-print("done")
+    print("done")
+
+
+if __name__ == "__main__":
+    process("data_lucas_XYZ_original.csv", "data_lucas_xyY_original.csv", "data_lucas_xyY.csv")
---
+++ prepare_dataset.py
@@ -0,0 +1,25 @@
+import one_time_convert_hsv
+import one_time_convert_hsv_xy
+import one_time_convert_XYZ
+import one_time_convert_xyY
+import one_time_convert_cielab
+
+
+def process(base):
+    rgb = f"data_{base}_rgb.csv"
+    hsv = f"data_{base}_hsv.csv"
+    hsv_xy = f"data_{base}_hsv_xy.csv"
+    XYZ_original = f"data_{base}_XYZ_original.csv"
+    XYZ = f"data_{base}_XYZ.csv"
+    xyY_original = f"data_{base}_xyY_original.csv"
+    xyY = f"data_{base}_xyY.csv"
+    cielab_original = f"data_{base}_cielab_original.csv"
+    cielab = f"data_{base}_cielab.csv"
+
+    one_time_convert_hsv.process(rgb, hsv)
+    one_time_convert_hsv_xy.process(hsv, hsv_xy)
+    one_time_convert_XYZ.process(hsv, hsv_xy)
+
+
+if __name__ == "__main__":
+    process("lucas")
