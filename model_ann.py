3afbceea4b59fbd01ed27127b55ba5aee28b74f6
Arif <80994072+arf-themascoteers@users.noreply.github.com>
2023-02-12 03:17:44 +1100

    done

--- model_ann.py
+++ model_ann.py
@@ -1,13 +1,19 @@
 import torch.nn as nn
+## 100X5X1 -> 0.710
+## 100X50X1 -> 0.727
+## 50X10X5X1 -> 0.722
+##
 
 
 class ANN(nn.Module):
-    def __init__(self, size=3, mid=100):
+    def __init__(self, size=3, mid=50):
         super().__init__()
         self.fc = nn.Sequential(
             nn.Linear(size, mid),
             nn.LeakyReLU(),
-            nn.Linear(mid, 1)
+            nn.Linear(mid,5),
+            nn.LeakyReLU(),
+            nn.Linear(5, 1)
         )
 
     def forward(self, x):
--- special_cases/nn-hsv-si.py
+++ special_cases/nn-hsv-si.py
@@ -7,14 +7,14 @@ import ds_manager
 if __name__ == "__main__":
     sis = ds_manager.DSManager.si_list()
     colour_space_models = ["hsv"]
-    colour_space_models.append({"cspace":"hsv","si":sis,"name":"hsv-si"})
-    colour_space_models.append({"cspace":"hsv","si":sis,"si_only":True,"name":"si"})
+    # colour_space_models.append({"cspace":"hsv","si":sis,"name":"hsv-si"})
+    # colour_space_models.append({"cspace":"hsv","si":sis,"si_only":True,"name":"si"})
 
     ev = Evaluator(
         datasets=["lucas"],
         algorithms=["nn"],
         colour_space_models=colour_space_models,
-        prefix="nn-hsv-si",
+        prefix="nn-hsv-si5",
         verbose=True,
         folds=10,
         repeat=1
