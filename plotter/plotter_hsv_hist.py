6eda2c118e442ee34b45b761e223abb54e99fddb
Arif <80994072+arf-themascoteers@users.noreply.github.com>
2023-02-22 12:33:43 +1100

    IGNORED

--- plotter/plotter_hsv_hist.py
+++ plotter/plotter_hsv_hist.py
@@ -6,48 +6,31 @@ import os
 
 os.chdir("../")
 
-# rgbs = pd.read_csv(f"data/ossl_rgb_raw.csv").to_numpy()
-# print(f"r: {np.round(np.min(rgbs[:, 0]), 3)}\t{np.round(np.max(rgbs[:, 0]), 3)}\t{np.round(np.mean(rgbs[:, 0]), 3)}")
-# print(f"g: {np.round(np.min(rgbs[:, 1]), 3)}\t{np.round(np.max(rgbs[:, 1]), 3)}\t{np.round(np.mean(rgbs[:, 1]), 3)}")
-# print(f"b: {np.round(np.min(rgbs[:, 2]), 3)}\t{np.round(np.max(rgbs[:, 2]), 3)}\t{np.round(np.mean(rgbs[:, 2]), 3)}")
-#
-# exit()
+for d in ["lucas", "raca", "ossl"]:
+    rgbs = pd.read_csv(f"data/{d}/hsv.csv").to_numpy()
 
-# for ds in ["lucas", "raca", "ossl"]:
-#     for cs in ["rgb", "hsv", "cielab"]:
-#         rgbs = pd.read_csv(f"data/{ds}/{cs}.csv").to_numpy()
-#         print(f"{ds} {cs}")
-#         print(f"1: {np.round(np.min(rgbs[:,0]),3)}\t{np.round(np.max(rgbs[:,0]),3)}\t{np.round(np.mean(rgbs[:,0]),3)}")
-#         print(f"2: {np.round(np.min(rgbs[:,1]),3)}\t{np.round(np.max(rgbs[:,1]),3)}\t{np.round(np.mean(rgbs[:,1]),3)}")
-#         print(f"3: {np.round(np.min(rgbs[:,2]),3)}\t{np.round(np.max(rgbs[:,2]),3)}\t{np.round(np.mean(rgbs[:,2]),3)}")
-#
-#
-# exit(0)
+    #plt.xlim(0,1)
+    fig, ax = plt.subplots(1,3)
 
-rgbs = pd.read_csv("data/ossl/hsv.csv").to_numpy()
+    hue = rgbs[:,0]
+    counts, bins = np.histogram(hue, bins=200)
+    ax[0].hist(bins[:-1], bins, weights=counts)
+    ax[0].set_title('Hue')
+    ax[0].set_xlim([0, 1])
 
-#plt.xlim(0,1)
-fig, ax = plt.subplots(1,3)
+    saturation = rgbs[:,1]
+    counts, bins = np.histogram(saturation, bins=200)
+    ax[1].hist(bins[:-1], bins, weights=counts)
+    ax[1].set_title('Saturation')
+    ax[1].set_xlim([0, 1])
 
-hue = rgbs[:,0]
-counts, bins = np.histogram(hue)
-ax[0].hist(bins[:-1], bins, weights=counts)
-ax[0].set_title('Hue')
-ax[0].set_xlim([0, 1])
+    value = rgbs[:,2]
+    counts, bins = np.histogram(value, bins=200)
+    ax[2].hist(bins[:-1], bins, weights=counts)
+    ax[2].set_title('Value')
+    ax[2].set_xlim([0, 1])
 
-saturation = rgbs[:,1]
-counts, bins = np.histogram(saturation)
-ax[1].hist(bins[:-1], bins, weights=counts)
-ax[1].set_title('Saturation')
-ax[1].set_xlim([0, 1])
-
-value = rgbs[:,2]
-counts, bins = np.histogram(value)
-ax[2].hist(bins[:-1], bins, weights=counts)
-ax[2].set_title('Value')
-ax[2].set_xlim([0, 1])
-
-fig.tight_layout(pad=1.0)
+    fig.tight_layout(pad=1.0)
 
 
-plt.show()
+    plt.show()
